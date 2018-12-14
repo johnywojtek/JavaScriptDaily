fff1a23e1b116f8769a506279022c341845f56bb
johnywojtek <johnywojtek@gmail.com>
2018-12-14 22:48:49 +0100

    daily

---
+++ 22 - Follow Along Link Highlighter/js/script.js
@@ -0,0 +1,17 @@
+const trigger = document.querySelectorAll('a');
+const highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.append(highlight);
+function highlightLink() {
+    const linkCoord = this.getBoundingClientRect();
+    const coords = {
+        width: linkCoord.width,
+        height: linkCoord.height,
+        top: linkCoord.top + window.scrollY,
+        left: linkCoord.left + window.scrollX
+    };
+    highlight.style.width = `${coords.width}px`;
+    highlight.style.height = `${coords.height}px`;
+    highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+trigger.forEach(e => e.addEventListener('mouseenter', highlightLink));
